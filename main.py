5104d498478f30e7b6cf956e3bcb3311d1ccf795
YezhenWang <309426989@qq.com>
2019-03-06 21:43:38 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,162 @@
+import sys
+import numpy as np
+import random
+from utils import *
+from model import CSRNet
+import torchvision.transforms as transforms
+from DatasetConstructor import DatasetConstructor
+import metrics
+from PIL import Image
+MAE = 10240000
+MSE = 10240000
+SHANGHAITECH = "B"
+# data_load
+img_dir = "/home/zzn/part_" + SHANGHAITECH + "_final/train_data/images"
+gt_dir = "/home/zzn/part_" + SHANGHAITECH + "_final/train_data/gt_map"
+transform_a = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+            ])
+gt_transform_a =  transforms.ToTensor()
+
+crop_height = 384
+crop_weight = 512
+
+default_transform = transforms.Compose([
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
+        transforms.TenCrop((crop_height, crop_weight), vertical_flip=False),
+        transforms.Lambda(
+            lambda crops: [
+                transforms.ToTensor()(crop) for crop in crops
+            ]
+        ),
+        transforms.Lambda(
+            lambda crops: [
+                transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))(crop) for crop in crops
+            ]
+        ),
+        transforms.Lambda(lambda crops: torch.stack(crops))
+    ])
+
+gt_transform = transforms.Compose([
+        transforms.TenCrop((crop_height, crop_weight), vertical_flip=False),
+        transforms.Lambda(
+            lambda crops: [
+                transforms.ToTensor()(crop) for crop in crops
+            ]
+        ),
+        transforms.Lambda(lambda crops: torch.stack(crops))
+    ])
+
+dataset = DatasetConstructor(img_dir, gt_dir, 400, 20, default_transform, gt_transform)
+train_loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=1)
+eval_loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=1)
+# obtain the gpu device
+assert torch.cuda.is_available()
+cuda_device = torch.device("cuda")
+
+# model construct
+net = CSRNet().to(cuda_device)
+gt_map_process_model = GroundTruthProcess(1, 1, 8).to(cuda_device) # to keep the same resolution with the prediction
+
+# set optimizer and estimator
+criterion = metrics.Loss().to(cuda_device)
+optimizer = torch.optim.Adam(net.parameters(), lr=1e-5, weight_decay=0.95)
+ae_batch = metrics.AEBatch().to(cuda_device)
+se_batch = metrics.SEBatch().to(cuda_device)
+for epoch_index in range(500):
+    dataset = dataset.train_model().shuffle()
+    # train
+    step = 0
+    for train_img_index, train_img, train_gt, data_ptc in train_loader:
+        # eval per 100 batch
+        if step % 100 == 0:
+            net.eval()
+            dataset = dataset.eval_model().shuffle()
+            loss_ = []
+            MAE_ = []
+            MSE_ = []
+
+            rand_number = random.randint(0, 19)
+            counter = 0
+
+            for eval_img_index, eval_img, eval_gt, eval_data_ptc in eval_loader:
+
+                # B
+                eval_x = eval_img.view(-1, 3, 768, 1024).cuda()
+                eval_y = eval_gt.view(-1, 1, 768, 1024).cuda()
+                # A
+                #                     eval_x = eval_img.cuda()
+                #                     eval_y = eval_gt.cuda()
+                eval_prediction = net(eval_x)
+                eval_groundtruth = gt_map_process_model(eval_y)
+                # That’s because numpy doesn’t support CUDA,
+                # so there’s no way to make it use GPU memory without a copy to CPU first.
+                # Remember that .numpy() doesn’t do any copy,
+                # but returns an array that uses the same memory as the tensor
+                eval_loss = criterion(eval_prediction, eval_groundtruth).data.cpu().numpy()
+                batch_ae = ae_batch(eval_prediction, eval_groundtruth).data.cpu().numpy()
+                batch_se = se_batch(eval_prediction, eval_groundtruth).data.cpu().numpy()
+
+                # random show 1 sample
+                if rand_number == counter:
+                    origin_image = Image.open("/home/zzn/part_" + SHANGHAITECH + "_final/train_data/images/IMG_" + str(
+                        eval_img_index.numpy()[0]) + ".jpg")
+                    validate_pred_map = np.squeeze(eval_prediction.permute(0, 2, 3, 1).data.cpu().numpy())
+                    validate_gt_map = np.squeeze(eval_groundtruth.permute(0, 2, 3, 1).data.cpu().numpy())
+
+                    show(origin_image, validate_gt_map, validate_pred_map, eval_img_index.numpy()[0])
+                    gt_counts = np.sum(validate_gt_map)
+                    pred_counts = np.sum(validate_pred_map)
+                    sys.stdout.write(
+                        'The gt counts of the above sample:{}, and the pred counts:{}\n'.format(gt_counts, pred_counts))
+
+                loss_.append(eval_loss)
+                MAE_.append(batch_ae)
+                MSE_.append(batch_se)
+                counter += 1
+
+            # calculate the validate loss, validate MAE and validate RMSE
+            loss_ = np.reshape(loss_, [-1])
+            MAE_ = np.reshape(MAE_, [-1])
+            MSE_ = np.reshape(MSE_, [-1])
+
+            validate_loss = np.mean(loss_)
+            validate_MAE = np.mean(MAE_)
+            validate_RMSE = np.sqrt(np.mean(MSE_))
+
+            sys.stdout.write(
+                'In step {}, epoch {}, with loss {}, MAE = {}, MSE = {}\n'.format(step, epoch_index + 1, validate_loss,
+                                                                                  validate_MAE, validate_RMSE))
+            sys.stdout.flush()
+
+            # save model
+            if MAE > validate_MAE:
+                MAE = validate_MAE
+                torch.save(net, "/home/zzn/Downloads/CSRNet_pytorch-master/checkpoints/csrnet_mae_b.pkl")
+
+            # save model
+            if MSE > validate_RMSE:
+                MSE = validate_RMSE
+                torch.save(net, "/home/zzn/Downloads/CSRNet_pytorch-master/checkpoints/csrnet_mse_b.pkl")
+
+            torch.save(net, "/home/zzn/Downloads/CSRNet_pytorch-master/checkpoints/csrnet_in_time.pkl")
+
+            # return train model
+            net.train()
+            dataset = dataset.train_model()
+
+        net.train()
+        optimizer.zero_grad()
+        # B
+        x = train_img.view(-1, 3, 384, 512).cuda()
+        y = train_gt.view(-1, 1, 384, 512).cuda()
+        # A
+        #       x = train_img.cuda()
+        #       y = train_gt.cuda()
+        prediction = net(x)
+        groundtruth = gt_map_process_model(y)
+        loss = criterion(prediction, groundtruth)
+        loss.backward()
+        optimizer.step()
+        step += 1
---
+++ utils.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+
+class GroundTruthProcess(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size):
+        super(GroundTruthProcess, self).__init__()
+        self.kernel_size = kernel_size
+        self.kernel = torch.FloatTensor(torch.ones(out_channels, in_channels, kernel_size, kernel_size)).cuda()
+
+    def forward(self, x):
+        result = F.conv2d(x, self.kernel, bias=None, stride=self.kernel_size, padding=0)
+        return result
+
+
+def show(origin_map, gt_map, predict, index):
+    figure, (origin, gt, pred) = plt.subplots(1, 3, figsize=(20, 4))
+    origin.imshow(origin_map)
+    origin.set_title("origin picture")
+    gt.imshow(gt_map, cmap=plt.cm.jet)
+    gt.set_title("gt map")
+    pred.imshow(predict, cmap=plt.cm.jet)
+    pred.set_title("prediction")
+    plt.suptitle(str(index) + "th sample")
+    plt.show()
+    plt.close()
